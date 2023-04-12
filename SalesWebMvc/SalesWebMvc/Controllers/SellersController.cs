c9e48243be4ba514daed3c8579a2deca71a8b96f
Victor Hugo <victor_nascimento1996@hotmail.com>
2023-04-12 00:54:20 -0300

    Simple Create form

--- SalesWebMvc/SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/SalesWebMvc/Controllers/SellersController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models;
 using SalesWebMvc.Services;
 using System;
 using System.Collections.Generic;
@@ -22,6 +23,18 @@ namespace SalesWebMvc.Controllers
             var list = _sellerservice.FindAll();
 
             return View(list);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _sellerservice.Insert(seller);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
